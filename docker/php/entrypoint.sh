120b00c8189fe5a69ae027e02db5c310ee6db03b
Tony P <whitewolf071@gmail.com>
2023-10-07 15:55:34 +0300

    Nginx+PHP-FPM container update

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,69 +1,67 @@
 #!/bin/sh
 
+# inspired by https://gist.github.com/themightychris/51a63a1c2bf6f3be82c9e85481106fa0
+
 Red='\033[0;31m'          # Red
 Green='\033[0;32m'        # Green
 
 echo ""
-echo "***********************************************************"
-echo " Starting the 🚚TRU}{DR1V3R🚛 APP                          "
-echo "***********************************************************"
+echo "${Green}***********************************************************"
+echo "${Green} Starting the 🚚TRU}{DR1V3R🚛 APP                          "
+echo "${Green}***********************************************************"
 
 set -e
 
-## Check if the artisan file exists
-if [ -f /var/www/html/artisan ]; then
-    echo "${Green} artisan file found, creating laravel supervisor config..."
-
-    ##Create Laravel Scheduler process
-    TASK=/etc/supervisor/conf.d/laravel-worker.conf
-    touch $TASK
-    cat > "$TASK" <<EOF
-    [program:Laravel-scheduler]
-    process_name=%(program_name)s_%(process_num)02d
-    command=/bin/sh -c "while [ true ]; do (php /var/www/html/artisan schedule:run --verbose --no-interaction &); sleep 60; done"
-    autostart=true
-    autorestart=true
-    numprocs=1
-    user=www-data
-    stdout_logfile=/var/log/laravel_scheduler.out.log
-    redirect_stderr=true
-
-    [program:Laravel-worker]
-    process_name=%(program_name)s_%(process_num)02d
-    command=php /var/www/html/artisan queue:work --sleep=3 --tries=3
-    autostart=true
-    autorestart=true
-    numprocs=$LARAVEL_PROCS_NUMBER
-    user=www-data
-    redirect_stderr=true
-    stdout_logfile=/var/log/laravel_worker.log
-EOF
-echo  "${Green} Laravel supervisor config created"
-else
-    echo  "${Red} artisan file not found"
-fi
-
-## Check if the supervisor config file exists
-if [ -f /var/www/html/conf/worker/supervisor.conf ]; then
-    echo "additional supervisor config found"
-    cp /var/www/html/conf/worker/supervisor.conf /etc/supervisor/conf.d/supervisor.conf
-    else
-    echo "${Red} Supervisor.conf not found"
-    echo "${Green} If you want to add more supervisor configs, create config file in /var/www/html/conf/worker/supervisor.conf"
-    echo "${Green} Start supervisor with default config..."
-fi
-
-## Check if php.ini file exists
-if [ -f /var/www/html/conf/php/php.ini ]; then
-    cp /var/www/html/conf/php/php.ini $PHP_INI_DIR/conf.d/
-    echo "Custom php.ini file found and copied in  $PHP_INI_DIR/conf.d/"
-else
-    echo "Custom php.ini file not found"
-    echo "If you want to add a custom php.ini file, you add it in /var/www/html/conf/php/php.ini"
-fi
+## start nginx and php in the background, kill both on exit
+echo ""
+echo "${Green}***********************************************************"
+echo " Starting NGINX                                            "
+echo "${Green}***********************************************************"
+nginx -g 'daemon off;' 2>&1 &
+NGINX_PID=$!
 
 echo ""
-echo "**********************************"
-echo "     Starting Supervisord...     "
-echo "***********************************"
-supervisord -c /etc/supervisor/supervisord.conf
+echo "${Green}***********************************************************"
+echo "${Green} Starting PHP-FPM                                          "
+echo "${Green}***********************************************************"
+
+php-fpm --daemonize
+PHP_FPM_PID=$!
+
+## initialize config cache
+php artisan config:cache
+
+## watch nginx and php in an ongoing loop
+while :
+do
+    kill -0 $NGINX_PID 2> /dev/null
+    NGINX_STATUS=$?
+
+    kill -0 $PHP_FPM_PID 2> /dev/null
+    PHP_FPM_STATUS=$?
+
+    if [ "$TRAPPED_SIGNAL" = "false" ]; then
+        if [ $NGINX_STATUS -ne 0 ] || [ $PHP_FPM_STATUS -ne 0 ]; then
+            if [ $NGINX_STATUS -eq 0 ]; then
+                kill -15 $NGINX_PID;
+                wait $NGINX_PID;
+            fi
+            if [ $PHP_FPM_STATUS -eq 0 ]; then
+                kill -15 $PHP_FPM_PID;
+                wait $PHP_FPM_PID;
+            fi
+
+            exit 1;
+        fi
+    else
+       if [ $NGINX_STATUS -ne 0 ] && [ $PHP_FPM_STATUS -ne 0 ]; then
+            exit 0;
+       fi
+    fi
+
+    # trigger laravel scheduler every 60s
+    php artisan schedule:run --verbose --no-interaction &
+
+    sleep 60
+done
+exec "$@"
